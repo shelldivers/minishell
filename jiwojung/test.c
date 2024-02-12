9835ae81855705783cc55661c4df31ab2138f5ab
KenWR <skyqkqk1168@gmail.com>
2024-02-12 20:37:02 +0900

    정리

--- jiwojung/test.c
+++ jiwojung/test.c
@@ -1,15 +1,14 @@
-/* readline함수를 사용하기위한 헤더 */
-# include <readline/readline.h>
-
-/* add_history함수를 사용하기위한 헤더 */
-# include <readline/history.h>
-
 /* printf함수를 사용하기위한 헤더 */
-# include <stdio.h>
+#include <stdio.h>
 
 /* free함수를 사용하기위한 헤더 */
-# include <stdlib.h>
+#include <stdlib.h>
 
+/* readline함수를 사용하기위한 헤더 */
+#include <readline/readline.h>
+
+/* add_history함수를 사용하기위한 헤더 */
+#include <readline/history.h>
 /* readline함수를 활용하여 간단한 프로그램 작성하기 */
 int main(void)
 {
@@ -24,6 +23,7 @@ int main(void)
             printf("%s\n", str);/* 주소안에 문자열을 출력해보자 */
         else/* str = NULL 이라면 (EOF, cntl + D)*/
             break ;/* 반복문을 탈출해준다.*/
+		printf ("%d\n", rl_on_new_line());
 	/* add_history에 저장된 문자열은 up & down 방향키를 이용해 확인할수있다 */
         add_history(str);
 	/* 라인은 힙메모리에 저장되기때문에 다 사용한 메모리는 할당을 해제해줘야한다 */
