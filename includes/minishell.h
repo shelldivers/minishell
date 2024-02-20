edc2a8c8e196c3f48e7adec6c64f2f9d62029e3e
yhames <wjddn494@naver.com>
2024-02-20 16:01:42 +0900

    :seedling: add googletest

---
+++ includes/minishell.h
@@ -0,0 +1,14 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# ifdef __cplusplus
+extern "C" {
+# endif
+
+// here
+
+# ifdef __cplusplus
+}
+# endif
+
+#endif
---
+++ test/minishell_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "includes/minishell.h"
+
+TEST(minishell, test1)
+{
+	// here
+}
