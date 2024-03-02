109602beda4f66e328168d58a95adb13066fc82d
yhames <wjddn494@naver.com>
2024-03-02 16:58:07 +0900

    🎨 change 파일 구조

--- includes/ms_expand.h
+++ includes/ms_expand.h
@@ -7,13 +7,19 @@ extern "C" {
 
 #include "ft_bool.h"
 
+/* ms_expand.c */
 char	**ms_expand(char **args);
 
+/* ms_expand_qv.c */
 t_bool	ms_expand_qv(char **args);
 char	*ms_expand_qv_exchange(char *arg);
+
+/* ms_expand_qv_utils.c */
+t_bool	ms_expand_env_exchange(char **arg, int *index, char *value);
+t_bool	ms_expand_env(char **arg, int *index);
+void	ms_expand_escape(char *arg, int *index);
 int		ms_expand_quote(char *arg, int *index);
 void	ms_expand_dquote(char *arg, int *index);
-void	ms_expand_escape(char *arg, int *index);
 
 
 # ifdef __cplusplus
