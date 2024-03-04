8f8130ea1ab3be2e39f32e39823481c0019da31f
yhames <wjddn494@naver.com>
2024-03-04 18:52:18 +0900

    :art: add funcs in header

--- includes/ms_error.h
+++ includes/ms_error.h
@@ -4,5 +4,7 @@
 # include "ms_env.h"
 
 void	ms_puterror_cmd_arg(t_env *env, char *cmd, char *arg);
+void	ms_puterror_cmd(t_env *env, char *cmd);
+void	ms_puterror_arg(t_env *env, char *arg);
 
 #endif
