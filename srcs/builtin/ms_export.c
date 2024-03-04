376d8032037be165b9577d7736241693faac59ff
yhames <wjddn494@naver.com>
2024-03-04 20:49:39 +0900

    ♻️ change export use errno

--- srcs/builtin/ms_export.c
+++ srcs/builtin/ms_export.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_export.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 20:49:17 by jeongwpa          #+#    #+#             */
+/*   Updated: 2024/03/04 20:49:18 by jeongwpa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ms_builtin.h"
 #include "ms_env.h"
 #include "ms_error.h"
 #include <unistd.h>
 
-static t_bool	ms_export_env(t_env **env, char *str);
+static void	ms_export_env(t_env **env, char *str);
 
 int	ms_export(int argc, char **argv, t_env **env)
 {
@@ -23,17 +35,24 @@ int	ms_export(int argc, char **argv, t_env **env)
 	return (EXIT_SUCCESS);
 }
 
-static t_bool	ms_export_env(t_env **env, char *str)
+static void	ms_export_env(t_env **env, char *str)
 {
 	t_env	*node;
 
 	node = ms_str_to_env(str);
 	if (!node)
-		return (FALSE);
-	if (!ms_setenv(env, node->key, node->value))
-		ms_puterror_cmd_arg(*env, "export", node->key);
+	{
+		ms_puterror_cmd(*env, "export");
+		return ;
+	}
+	if (ms_is_valid_env_key(node->key) == FALSE)
+		ms_puterror_identifier(*env, "export", node->key);
+	else
+	{
+		if (!ms_setenv(env, node->key, node->value))
+			ms_puterror_cmd_arg(*env, "export", node->key);
+	}
 	free(node->key);
 	free(node->value);
 	free(node);
-	return (TRUE);
 }
