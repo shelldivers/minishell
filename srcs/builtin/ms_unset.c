616bbd1376be6cbc0fcb932d9f19976a95ece7b5
yhames <wjddn494@naver.com>
2024-03-04 20:01:13 +0900

    ♻️ change unset use errno

--- srcs/builtin/ms_unset.c
+++ srcs/builtin/ms_unset.c
@@ -1,23 +1,21 @@
-#include "minishell.h"
+#include "ms_builtin.h"
 #include "ms_env.h"
+#include "ms_error.h"
 #include <stdlib.h>
-#include <unistd.h>
 
 int	ms_unset(int argc, char **argv, t_env **env)
 {
 	int	status;
 
-	if (argc < 1 || argv == NULL || env == NULL)
-		return (EXIT_FAILURE);	// todo : error message
 	if (argc == 1)
-		return (EXIT_FAILURE); 	// todo : error message
+		return (EXIT_SUCCESS);
 	argv++;
 	status = EXIT_SUCCESS;
 	while (*argv)
 	{
 		if (ms_is_valid_env_key(*argv) == FALSE)
 		{
-			write(2, "unset: invalid parameter name\n", 30);	// todo : error message
+			ms_puterror_identifier(*env, "unset", *argv);
 			status = EXIT_FAILURE;
 		}
 		else
