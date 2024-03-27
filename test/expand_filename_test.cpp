778b77b2e42c7d9339ff4bb22f61273d38813a89
yhames <wjddn494@naver.com>
2024-03-27 16:30:29 +0900

    ❎ add failed test: BFS 동작 오류

--- test/expand_filename_test.cpp
+++ test/expand_filename_test.cpp
@@ -74,6 +74,44 @@ TEST(ms_filename_test, ms_filename_test3)
 		free(argv[i]);
 }
 
+TEST(ms_filename_test, ms_filename_test4)
+{
+	char *argv[3];
+	t_env **env;
+	char **result;
+
+	// given
+	char *envp[] = {
+		"PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki",
+		"PWD=/Users/jeongwpa",
+		"SHLVL=1",
+		"HOME=/Users/jeongwpa",
+		"LOGNAME=jeongwpa",
+		NULL,
+	};
+	env = ms_env_deserialize(envp);
+	argv[0] = ft_strdup("echo");
+	argv[1] = ft_strdup("u*/l*/a*");
+	argv[2] = NULL;
+
+	// when
+	result = ms_expand_filename(argv, *env);
+
+	// then
+	EXPECT_NE(result, nullptr);
+	for (int i = 0; result[i]; i++)
+		cout << "result: " << result[i] << endl;
+
+	// final
+	ms_env_clear(env);
+	free(env);
+	for (int i = 0; result[i]; i++)
+		free(result[i]);
+	free(result);
+	for (int i = 0; argv[i]; i++)
+		free(argv[i]);
+}
+
 TEST(ms_filename_test, ms_filename_test2)
 {
 	char *argv[3];
