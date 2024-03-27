1137a7f095996222ca680415d0b674f8b6abb365
yhames <wjddn494@naver.com>
2024-03-27 12:59:15 +0900

    :art: globbing

--- srcs/expand/ms_expand_glob2.c
+++ srcs/expand/ms_expand_glob2.c
@@ -6,34 +6,18 @@
 static char	*get_path_from_root(char *str);
 static char	*get_path_from_cur(char *str);
 
-char	*ms_get_path(char *str)
+t_bool	ms_get_path(t_glob *wc, char *str)
 {
-	if (*str == '/')
-		return (get_path_from_root(str));
-	else
-		return (get_path_from_cur(str));
-}
-
-char	*ms_get_pattern(char *str)
-{
-	size_t	i;
 	char	*tmp;
 
-	i = 0;
-	while (str[i] != '*')
-	{
-		if (str[i] != '/')
-			i++;
-		else
-		{
-			str = str + i + 1;
-			i = 0;
-		}
-	}
-	tmp = ft_strchr(str, '/');
-	if (!tmp || *(tmp + 1) == '\0')
-		return (ft_strdup(str));
-	return (ft_strndup(str, tmp - str));
+	if (*str == '/')
+		tmp = get_path_from_root(str);
+	else
+		tmp = get_path_from_cur(str);
+	if (!tmp)
+		return (FALSE);
+	wc->path = tmp;
+	return (TRUE);
 }
 
 static char	*get_path_from_root(char *str)
