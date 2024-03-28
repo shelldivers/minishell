b3246c5ccb520c9797c7dabf92aad68a22ec2835
yhames <wjddn494@naver.com>
2024-03-28 18:55:26 +0900

    ✅ add test

--- test/expand_filename_test.cpp
+++ test/expand_filename_test.cpp
@@ -5,12 +5,55 @@
 
 using namespace std;
 
+TEST(ms_filename_test, ms_filename_test16)
+{
+	char *argv[3];
+	t_env **env;
+	char **result;
+	char *input = "*u/././*";	// input
+
+	// given
+	char *envp[] = {
+		"PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/munki",
+		"PWD=/Users/jeongwpa",
+		"SHLVL=1",
+		"HOME=/Users/jeongwpa",
+		"LOGNAME=jeongwpa",
+		NULL,
+	};
+	env = ms_env_deserialize(envp);
+	argv[0] = ft_strdup("echo");
+	argv[1] = ft_strdup(input);
+	argv[2] = NULL;
+
+	// when
+	result = ms_expand_filenames(argv, *env);
+
+	// then
+	cout << endl;
+	EXPECT_NE(result, nullptr);
+	int count = 0;
+	for (int i = 0; result[i]; i++, count++)
+		cout << "result: " << result[i] << endl;
+	cout << endl;
+	cout << "count: " << count - 1 << endl;
+
+	// final
+	ms_env_clear(env);
+	free(env);
+	for (int i = 0; result[i]; i++)
+		free(result[i]);
+	free(result);
+	for (int i = 0; argv[i]; i++)
+		free(argv[i]);
+}
+
 TEST(ms_filename_test, ms_filename_test15)
 {
 	char *argv[3];
 	t_env **env;
 	char **result;
-	char *input = "*/..,";	// input
+	char *input = "*/arc";	// input
 
 	// given
 	char *envp[] = {
