e2e80e1a53c474780b7606af360ba62d882c5897
kenWR <skyqkqk1168@gmail.com>
2024-04-05 14:15:06 +0900

    ✅ remove nonuse variable

--- srcs/exec/ms_quote_removal_dup.c
+++ srcs/exec/ms_quote_removal_dup.c
@@ -6,7 +6,7 @@
 /*   By: jiwojung <jiwojung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:33:24 by jiwojung          #+#    #+#             */
-/*   Updated: 2024/04/05 14:13:10 by jiwojung         ###   ########.fr       */
+/*   Updated: 2024/04/05 14:14:46 by jiwojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ char	*ms_quote_removal_dup(char *word, size_t i, size_t j)
 {
 	char	quote;
 	char	*new_word;
-	char	*tmp;
 
 	new_word = (char *)malloc(sizeof(char) * (ft_strlen(word) + 1));
 	if (!new_word)
