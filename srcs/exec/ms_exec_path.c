e89e52b21d322b5cccfcd396cf9764662c8f9dd8
kenWR <skyqkqk1168@gmail.com>
2024-04-07 21:02:37 +0900

    🚧 add no files error

--- srcs/exec/ms_exec_path.c
+++ srcs/exec/ms_exec_path.c
@@ -6,7 +6,7 @@
 /*   By: jiwojung <jiwojung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 19:48:15 by jiwojung          #+#    #+#             */
-/*   Updated: 2024/04/07 15:46:09 by jiwojung         ###   ########.fr       */
+/*   Updated: 2024/04/07 21:01:27 by jiwojung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	ms_add_path(char **words, t_env **env)
 	ms_is_dir(words);
 	envp = ms_env_serialize(*env);
 	paths = ms_get_paths(envp);
-	if (!paths)
+	if (!paths || !paths[0])
 	{
 		ms_puterror_no_file(words[0]);
 		exit(127);
